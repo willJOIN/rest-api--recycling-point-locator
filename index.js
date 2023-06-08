3111067e03bc8daa7e77bfa76d81def49142fc2f
willJOIN <whirano4@gmail.com>
2023-06-07 21:56:11 -0300

    fix: altera port padrao

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(routePontosColeta);
 
-const port = 8080;
+const port = 3000;
 
 app.listen(port, () => {
     console.log("Servidor ativo na porta", port);
